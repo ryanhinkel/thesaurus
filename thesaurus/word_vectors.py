53564db5ff4177d4eb436ea42f7b8156b1521777
Patrick Smith <patrick@promptworks.com>
2018-12-17 17:09:46 -0500

    Add some debug print statements to load vector

--- thesaurus/word_vectors.py
+++ thesaurus/word_vectors.py
@@ -6,12 +6,16 @@ from thesaurus.config import filename
 from thesaurus.engine import engine
 
 
-def load_vectors():
-    print('loading vectors from {}'.format(filename))
+def load_vectors(debug=False):
+    if debug:
+        print('loading vectors from {}'.format(filename))
 
     lines = open(filename).read().strip().split('\n')
 
-    for line in lines:
+    for i, line in enumerate(lines):
+        if debug and i % 1_000 == 0:
+            print('done {} / {} ({}%)'.format(i, len(lines), 100.0 * float(i)/len(lines)))
+
         split_line = line.split()
         word = split_line[0]
         vec = array([float(thing) for thing in split_line[1:]])
@@ -23,4 +27,4 @@ def get_word(word):
 
 
 if __name__ == '__main__':
-    load_vectors()
+    load_vectors(debug=True)
